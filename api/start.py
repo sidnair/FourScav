0a0e277fc6d120aaeacab9ef9d70a580d95937a9
Eitan <lists@eitanadler.com>
2011-04-09 19:18:23 -0400

    add a slash

--- api/start.py
+++ api/start.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
 class auth:
 	def GET(self):
-		params = urllib.urlencode({'client_id' : 'DQCCND5KOFCIYVQXB3QX4GHJAR4AH4OHTQAM21JD0OFY4J00', 'client_secret' : 'MOBSNY4L5INCORUP1YPS4W3YYINAKSPWXLFSMZWYZUNNH4AE', 'grant_type' : 'authorization_code', 'redirect_uri' : 'http://localhost:8080/auth', 'code' : web.input().code })
+		params = urllib.urlencode({'client_id' : 'DQCCND5KOFCIYVQXB3QX4GHJAR4AH4OHTQAM21JD0OFY4J00', 'client_secret' : 'MOBSNY4L5INCORUP1YPS4W3YYINAKSPWXLFSMZWYZUNNH4AE', 'grant_type' : 'authorization_code', 'redirect_uri' : 'http://localhost:8080/auth/', 'code' : web.input().code })
 
 		opener = urllib.FancyURLopener({})
 		f = opener.open("https://foursquare.com/oauth2/access_token?%s" % params)

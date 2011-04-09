01ba04858fc734805c7aca25f19381d2e0e22a0a
Zhehao Mao <zhehao.mao@gmail.com>
2011-04-09 17:54:44 -0400

    removed .pyc

--- api/connection.py
+++ api/connection.py
@@ -2,3 +2,4 @@ from pymongo import Connection
 
 mongo = Connection('flame.mongohq.com', 27058)['4sqav']
 mongo.authenticate('4sqav', 'hacknyspring2011')
+
--- api/start.py
+++ api/start.py
@@ -6,4 +6,4 @@ urls = (
 app = web.application(urls, locals())
 
 if __name__ == '__main__':
-	app.run()
+    app.run()

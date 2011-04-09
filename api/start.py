ada96e880bc1e03015e18ee434d4d574ef497027
Eitan <lists@eitanadler.com>
2011-04-09 19:30:47 -0400

    return the token in a more usable form

--- api/start.py
+++ api/start.py
@@ -1,5 +1,6 @@
 import web
 import urllib
+import json
 
 urls = (
 	'/auth/', 'auth'
@@ -14,7 +15,11 @@ class auth:
 	def GET(self):
 		params = urllib.urlencode({'client_id' : 'DQCCND5KOFCIYVQXB3QX4GHJAR4AH4OHTQAM21JD0OFY4J00', 'client_secret' : 'MOBSNY4L5INCORUP1YPS4W3YYINAKSPWXLFSMZWYZUNNH4AE', 'grant_type' : 'authorization_code', 'redirect_uri' : 'http://localhost:8080/auth/', 'code' : web.input().code })
 
-		opener = urllib.FancyURLopener({})
-		f = opener.open("https://foursquare.com/oauth2/access_token?%s" % params)
-		return f.read()
+		hostname = "https://foursquare.com/oauth2/access_token?" + params
+		f = urllib.urlopen(hostname)
+		accReponse = f.read()
+		accDict = json.loads(accResponse)
+		accToken = accDist["access_token"]
+		return accToken 
+
 

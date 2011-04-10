9e82238e845d05f916a6e17c5dc6ccaa6561c5c6
Zhehao Mao <zhehao.mao@gmail.com>
2011-04-09 20:58:19 -0400

    Added session handling

--- api/start.py
+++ api/start.py
@@ -1,12 +1,16 @@
 import web
+from web.session import Session, DiskStore
 import urllib
 import json
+from models import User
 
 urls = (
 	'/auth/', 'auth'
 )
 
 app = web.application(urls, locals())
+
+session = web.session.Session(app, Diskstore('../sessions'))
 
 if __name__ == '__main__':
 	app.run()
@@ -23,6 +27,7 @@ class auth:
 		if accToken == None:
 			return "ID 10 T error"
 		else:
+			session.token = accToken
 			return "Congrats - you logged in"
 
 

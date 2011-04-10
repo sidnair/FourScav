ba362148f5cdce3f03454a2093d9e5cab85ab24f
Zhehao Mao <zhehao.mao@gmail.com>
2011-04-10 01:30:14 -0400

    moving helper functions to helper.py

---
+++ api/helpers.py
@@ -0,0 +1,16 @@
+def add_place(list_id, fsq_id):
+	oauth = None
+	hostname = "https://api.foursquare.com/v2/venues/" + fsq_id + "?" + oauth #oauth token
+	f = urllib.urlopen(hostname)
+	accResponse = f.read()
+	accDict = json.loads(accResponse)
+	accName = accDict.get("name")
+	accLat = accDict.get("location").get("lat")
+	accLong = accDict.get("location").get("long")
+	accDesc = accDict.get("description")
+	accTags = accDict.get("tags")
+	place = Place(name = accName,desc = accDesc, tags = accTags, geo_lat = accLat, 
+					geo_long = accLong)
+	place.save()
+	
+def expand_place():

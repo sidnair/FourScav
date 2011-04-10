a6f63c8826f71e1d48aaa86eecd4bff98e7ea51d
Sid Nair <ssn2114@columbia.edu>
2011-04-10 03:10:25 -0400

    added splash js file

---
+++ static/js/splash.js
@@ -0,0 +1,4 @@
+$('#loginButtonLink').click(function() {
+    window.location.href='https://foursquare.com/oauth2/authenticate?client_id=DQCCND5KOFCIYVQXB3QX4GHJAR4AH4OHTQAM21JD0OFY4J00&response_type=code&redirect_uri=http://localhost:8080/auth/'
+});
+$('#loginButtonLink').addClass('clickable');
